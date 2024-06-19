ad83fc694c2335448c062aa9fd1d9b0a06624b63
ProjectRavel <rafaelcodeid@gmail.com>
2024-06-19 19:52:41 +0700

    Membuat Project Counting

--- 02-Counting/app.js
+++ 02-Counting/app.js
@@ -1,18 +1,18 @@
-const decrease = document.querySelector('.decrease')
-const reset = document.querySelector('.reset')
-const increase = document.querySelector('.increase')
-const buttonContainer = document.querySelector('.button-container')
-const count = document.querySelector('#value')
-let counts = 0
-count.innerHTML = counts
+const btns = document.querySelectorAll('.btn')
+const value = document.querySelector('#value')
+let values = 0
 
-buttonContainer.addEventListener('click', (e)=>{
-    
-    if(e.target.classList.contains('decrease')){
-        count.innerHTML--
-    }if(e.target.classList.contains('increase')){
-        count.innerHTML++
-    }if(e.target.classList.contains('reset')){
-        count.innerHTML = 0
-    }
+btns.forEach((e)=>{
+    e.addEventListener('click', (eventB)=>{
+        if(e.classList.contains('decrease')){
+            values = values - 1
+            value.innerHTML = values
+        }else if(e.classList.contains('increase')){
+            values++
+            value.innerHTML= values
+        }else{
+            values = 0
+            value.innerHTML= values
+        }
+    })
 })
